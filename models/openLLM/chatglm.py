82cbb04072cb5645001381b2e0193819ae274651
Liuxyly <cenguor@gmail.com>
2023-06-08 10:38:15 +0800

    Update chatglm.py

--- models/openLLM/chatglm.py
+++ models/openLLM/chatglm.py
@@ -9,13 +9,18 @@ class ChatGLM(LLM):
   temperature: float = 0.1
   top_p: float = 0.9
   history = []
+  
+  def __init__(self, path_or_repo = "THUDM/chatglm-6b"):
+    from transformers import AutoModel, AutoTokenizer
+    self.tokenizer = AutoTokenizer.from_pretrained(path_or_repo, trust_remote_code = True, local_files_only=True)
+    self.model = AutoModel.from_pretrained(path_or_repo, trust_remote_code = True, local_files_only=True).half().cuda().eval()
 
   @property
   def _llm_type(self) -> str:
     return "ChatGLM"
   
   def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
-    response, history = model.chat(tokenizer, prompt, history=self.history, max_length=self.max_token, top_p=self.top_p, temperature=self.temperature)
+    response, history = self.model.chat(self.tokenizer, prompt, history=self.history, max_length=self.max_token, top_p=self.top_p, temperature=self.temperature)
     if stop is not None:
       response = enforce_stop_tokens(response, stop)
     self.history = self.history + [[None, response]]
